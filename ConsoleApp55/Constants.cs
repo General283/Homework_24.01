84ca1d62e239c70cb0d0cbc7affca351e7811792
RespectAssasin <mihafuntt@gmail.com>
2024-01-24 19:49:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp55/Constants.cs
@@ -0,0 +1,5 @@
+﻿public class Constants
+{
+    public const string DatabaseFile = "@C:\Users\student.TOP\Desktop\Group_project\ConsoleApp55\db.sqlite";
+    public const string DatabaseConnectionString = "Data Source=" + DatabaseFile;
+}
---
+++ ConsoleApp55/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.Sqlite;
+
+File.Create(Constants.DatabaseFile).Close();
+
+using (SqliteConnection databaseConnection = new SqliteConnection(Constants.DatabaseConnectionString))
+{
+    databaseConnection.Open();
+
+    using (var command = databaseConnection.CreateCommand())
+    {
+        command.CommandType = System.Data.CommandType.Text;
+
+        command.CommandText = "CREATE TABLE Invest( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, surname TEXT, address TEXT, email TEXT, phone TEXT, registration_date DATE);";
+        command.ExecuteNonQuery();
+        command.CommandText = "CREATE TABLE Money(money_id INTEGER PRIMARY KEY AUTOINCREMENT, Money_name TEXT, description TEXT, creation_date DATE, Money_size REAL,current_assets REAL);";
+        command.ExecuteNonQuery();
+        command.CommandText = "CREATE TABLE Transact(transact_id INTEGER PRIMARY KEY AUTOINCREMENT, investor_id INTEGER  Invest(investor_id), money_id INTEGER  Money(money_id), Transact_type TEXT, Transact_amount REAL, Transact_date DATE);";
+        command.ExecuteNonQuery();       
+
+        command.ExecuteNonQuery();
+    }
+
+    databaseConnection.Close();
+}
+
